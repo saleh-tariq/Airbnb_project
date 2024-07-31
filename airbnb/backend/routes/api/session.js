9f009ef30cf2f698c3e4036c7d1e96fcfe67f86b
saleh-tariq <tsaleh2464@gmail.com>
2024-07-31 19:43:02 -0400

    Add User logout backend endpoint

--- airbnb/backend/routes/api/session.js
+++ airbnb/backend/routes/api/session.js
@@ -8,8 +8,6 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
-// ...
-
 // Log in
 router.post("/", async (req, res, next) => {
   const { credential, password } = req.body;
@@ -42,6 +40,12 @@ router.post("/", async (req, res, next) => {
   return res.json({
     user: safeUser,
   });
+});
+
+// Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
 });
 
 module.exports = router;
