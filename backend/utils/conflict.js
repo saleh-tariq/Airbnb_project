9206a4bb199ee680c9545155995b07cd6541c15d
saleh-tariq <tsaleh2464@gmail.com>
2024-08-04 06:24:51 -0400

    added booking conflict check

--- backend/utils/conflict.js
+++ backend/utils/conflict.js
@@ -1,3 +1,23 @@
-function checkConflict(booking) {
+const { Booking, Spot } = require("../db/models");
+async function checkConflict(booking) {
   const { spotId, startDate, endDate } = booking;
+  const spot = await Spot.findOne({
+    where: { id: spotId },
+    include: [Booking],
+  });
+  const bookings = spot.Bookings;
+  const bookingTimeline = [];
+  for (let i = 0; i < booking.length; i++) {
+    const [min, max, start, end] = [
+      new Date(booking[i].startDate),
+      new Date(booking[i].endDate),
+      new Date(startDate),
+      new Date(endDate),
+    ];
+    if ((start >= min && start <= max) || (end >= min, end <= max)) {
+      return false;
+    }
+  }
+  return true;
 }
+module.exports = checkConflict;
