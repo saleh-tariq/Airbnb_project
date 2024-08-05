a5d332a87408cf95bb22dd27d28b0f62fdc378f3
James-Jon27 <jamescjones818@gmail.com>
2024-08-05 11:13:28 -0600

    Delete Spot image

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -8,18 +8,18 @@ const { handleValidationErrors } = require("../../utils/validation");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 
-const { Review, User, Spot, ReviewImage } = require("../../db/models");
+const { SpotImage, Spot } = require("../../db/models");
 
 router.delete("/:imageId", requireAuth, async (req, res) => {
-	const img = await ReviewImage.findByPk(req.params.imageId);
+	const img = await SpotImage.findByPk(req.params.imageId);
 	if (!img) {
 		return res.status(404).json({
-			message: "Review Image couldn't be found",
+			message: "Spot Image couldn't be found",
 		});
 	}
 
-	const review = await Review.findByPk(img.reviewId);
-	if (req.user.id !== review.userId) {
+    const spot = await Spot.findByPk(img.spotId);
+	if (req.user.id !== spot.ownerId) {
 		return res.status(403).json({
 			message: "Forbidden",
 		});
