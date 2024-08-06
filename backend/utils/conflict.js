e62acd45526438528e5ee6ad247993943b368f9b
saleh-tariq <tsaleh2464@gmail.com>
2024-08-06 11:21:20 -0400

    adjusted booking conflict checker

--- backend/utils/conflict.js
+++ backend/utils/conflict.js
@@ -14,11 +14,15 @@ async function checkConflict(booking) {
       new Date(startDate).getTime(),
       new Date(endDate).getTime(),
     ];
+    if (start === end) {
+      res.startDate = "Start date conflicts with an existing booking";
+      res.endDate = "End date conflicts with an existing booking";
+    }
     if (start >= min && start <= max) {
-      res.startDate = "Start date conflicts with and existing booking";
+      res.startDate = "Start date conflicts with an existing booking";
     }
     if (end >= min && end <= max) {
-      res.endDate = "End date conflicts with and existing booking";
+      res.endDate = "End date conflicts with an existing booking";
     }
   }
   return res.startDate || res.endDate ? res : false;
