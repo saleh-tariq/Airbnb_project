60904eb4ddc718026d20d090a0bb511ce340d0b5
saleh-tariq <tsaleh2464@gmail.com>
2024-08-26 18:38:34 -0400

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
+import Navigation from "./components/Navigation/Navigation";
 import * as sessionActions from "./store/session";
 
 function Layout() {
@@ -15,7 +16,12 @@ function Layout() {
     });
   }, [dispatch]);
 
-  return <>{isLoaded && <Outlet />}</>;
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
 }
 
 const router = createBrowserRouter([
@@ -27,11 +33,11 @@ const router = createBrowserRouter([
         element: <h1>Welcome!</h1>,
       },
       {
-        path: "/login",
+        path: "login",
         element: <LoginFormPage />,
       },
       {
-        path: "/signup",
+        path: "signup",
         element: <SignupFormPage />,
       },
     ],
